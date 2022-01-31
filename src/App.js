53ba58541b91fbcca6221e143edd1159026fd660
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-31 10:44:39 +0100

    Einige Änderungen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Envelope from "./components/envelope";
 function App() {
     return (
         <div className="App">
-            <Envelope fromPerson={"Mr.Sender"} addressLabel1={"Boston, MA 02118"} toPerson={"Mrs.Rocolver"} addressLabel2={"San Francisco,CA 94101"} stample={"stample.jpeg"}/>
+            <Envelope fromPerson={"Mr.Sender"} addressLabel1={"Boston, MA 02118"} stadt1={"123 Fake St."}  toPerson={"Mrs.Rocolver"} addressLabel2={"San Francisco,CA 94101"} stadt2={"123 Fake St."} stample={"stample.jpeg"}/>
 
         </div>
     );
--- src/components/envelope.js
+++ src/components/envelope.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./envelopeStyle.css";
 
-function Envelope({fromPerson , addressLabel1,toPerson , addressLabel2 , stample}) {
+function Envelope({fromPerson , addressLabel1, stadt1,toPerson , addressLabel2,stadt2 , stample}) {
   return (
     <div className="Envelope">
     <div className="Container-1">
       <h3 className="fromPerson">{fromPerson}</h3>
-      <h3>123 Fake St.</h3>
+      <h3>{stadt1}</h3>
       <h3 className="Address1">{addressLabel1}</h3>
       </div>
 
       <div className = "Container-2" >
         <h3 className="toPerson">{toPerson}</h3>
-        <h3>123 Fake St.</h3>
+        <h3>{stadt2}</h3>
         <h3 className="Address2">{addressLabel2}</h3>
       </div>
       <img  className="Stample" src={stample} alt="stample"/>
